d1d975ec0e893537328a7f97e537467377ece3b3
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-09-08 11:28:39 -0600

    debugging

--- Scripts/Event/WTUA;~!~!~!~.js
+++ Scripts/Event/WTUA;~!~!~!~.js
@@ -1,7 +1,7 @@
 if (appMatch("Building/*/*/*") && wfTask == "Application Intake" && wfStatus == "Denied") {
 	showDebug = 3
 	comment("CLOSE WORKFLOW")
-	deactivateTask("Application Intake")
+	deactivateTask("Application Intake");
+	include("WTUA;Building!Permit!Agency Clearance!Commercial");
 }
 
-include("WTUA;Building!Permit!Agency Clearance!Commercial");

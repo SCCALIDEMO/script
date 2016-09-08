c42b9b82475dd4942b794f71817dab463c0e0a93
Bryan de Jesus <bryan.dejesus@woolpert.com>
2016-09-08 09:47:09 -0600

    added script criteria

--- Scripts/Event Scripts/createAgencyClearanceChildRecord.js
+++ Scripts/Event Scripts/createAgencyClearanceChildRecord.js
@@ -11,21 +11,23 @@
 ===================================================================*/
 logDebug("running script: createAgencyClearanceChildRecord");
 try {
-	var childId = null;
-	var structType = AInfo["Building/Structure Type"];
-	if (structType == "Commercial" || structType == "Mixed Use (residential and commercial)"){
-		// Create Child Case Building/Permit/Agency Clearance/Commercial 
-		childId = createChild("Building", "Permit", "Agency Clearance", "Commercial", "Agency Clearance for " + capId.getCustomID());
-	} else if (structType == "Residential (apartment, condo, hotel/motel, other residential)" || structType == "Residential (single-family, duplex, or townhome)"){
-		// Create Child Case Building/Permit/Agency Clearance/Residential
-		childId = createChild("Building", "Permit", "Agency Clearance", "Residential", "Agency Clearance for " + capId.getCustomID());
-	} 
-	
-	if (childId != null){
-		// Copy Applicant, Contacts, Address, Parcel, Owner, and Description of Work information from Record Detail in parent case to new child case
-		// NOTE: createChild already copies Parcel, Contacts, and Addresses
-		copyOwner(capId, childId);
-		aa.cap.copyCapWorkDesInfo(capId, childId);
+	if (wfTask == "Building and Safety Clearance for Plan Check" && wfStatus == "Initiate Agency Clearance"){
+		var childId = null;
+		var structType = AInfo["Building/Structure Type"];
+		if (structType == "Commercial" || structType == "Mixed Use (residential and commercial)"){
+			// Create Child Case Building/Permit/Agency Clearance/Commercial 
+			childId = createChild("Building", "Permit", "Agency Clearance", "Commercial", "Agency Clearance for " + capId.getCustomID());
+		} else if (structType == "Residential (apartment, condo, hotel/motel, other residential)" || structType == "Residential (single-family, duplex, or townhome)"){
+			// Create Child Case Building/Permit/Agency Clearance/Residential
+			childId = createChild("Building", "Permit", "Agency Clearance", "Residential", "Agency Clearance for " + capId.getCustomID());
+		} 
+		
+		if (childId != null){
+			// Copy Applicant, Contacts, Address, Parcel, Owner, and Description of Work information from Record Detail in parent case to new child case
+			// NOTE: createChild already copies Parcel, Contacts, and Addresses
+			copyOwner(capId, childId);
+			aa.cap.copyCapWorkDesInfo(capId, childId);
+		}
 	}
 } catch (error){
 	logDebug("Javascript error: " + error.message);
